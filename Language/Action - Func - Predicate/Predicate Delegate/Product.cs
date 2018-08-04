54679d040aeb40daae7dd1d931e646d20a98cadd
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-08-04 10:20:17 +0530

    Add Actoin-Fund-Predicate examples

---
+++ Language/Action - Func - Predicate/Predicate Delegate/Product.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Learn.CSharp.Language
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+           
+        public static List<Product> GetSampleProducts()
+        {
+            return new List<Product> 
+            {
+                new Product { Id = 1, Name = "Surface" },
+                new Product { Id = 2, Name = "iPad" },
+                new Product { Id = 3, Name = "iPod" },
+                new Product { Id = 4, Name = "Sumsung Tablet" },
+                new Product { Id = 5, Name = "Digital Camera" },
+                new Product { Id = 6, Name = "Kindle Reader" }
+            };
+        }
+
+        public override string ToString()
+        {
+            return this.Id + " " + this.Name;
+        }
+    }
+
+}
---
+++ Language/Action - Func - Predicate/Predicate Delegate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Learn.CSharp.Language
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Predicate<double> isEvenNumber = d => d % 2 == 0;
+            Console.WriteLine("10 is even number? - " + isEvenNumber(10));
+            Console.WriteLine("11 is even number? - " + isEvenNumber(11));
+            Console.WriteLine();
+
+            Console.WriteLine("Product example");
+
+            var products = Product.GetSampleProducts();
+            Predicate<Product> iProducts = delegate(Product p) { return p.Name.StartsWith("i"); };
+            var matches = products.FindAll(iProducts);
+            Action<Product> print = Console.WriteLine;
+            matches.ForEach(print);
+
+            Console.ReadLine();
+        }
+    }
+}
