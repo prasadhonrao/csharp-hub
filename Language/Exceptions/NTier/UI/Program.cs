502b8ef666b6fef96213747e5bb9701e65767766
Prasad Honrao <Honrao.Prasad@Gmail.com>
2018-10-07 18:53:26 +0530

    Files renamed

--- Language/Exceptions/NTier/UI/Program.cs
+++ Language/Exceptions/NTier/UI/Program.cs
@@ -6,17 +6,7 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            try
-            {
-                string x = null;
-                int a = 10;
-
-                Console.WriteLine(int.Parse(x) + a);
-            }
-            catch (Exception ex)
-            {
-                ExceptionHandler.HandleException(ex);
-            }
+            Console.ReadLine();
         }
        
     }
