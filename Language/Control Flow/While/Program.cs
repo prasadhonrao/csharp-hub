d36a23e87198b37423a208166fd0ade7b9f1c688
prasadhonrao <honrao.prasad@gmail.com>
2024-01-09 16:37:18 +0000

    Update while loop example

--- Language/Control Flow/While/Program.cs
+++ Language/Control Flow/While/Program.cs
@@ -1,30 +1,21 @@
-﻿using System;
-using static System.Console;
+﻿using static System.Console;
 
-namespace Learn.CSharp.ControlFlow
+Console.WriteLine("While Loop!");
+
+Write("Enter a number to calculate factorial : ");
+int inputNumber = int.Parse(ReadLine());
+
+WriteLine("{0} factorial is {1}", inputNumber, Factorial(inputNumber));
+
+static double Factorial(int input)
 {
-    class Program
+    double fact = 1;
+
+    while (input > 0)
     {
-        static void Main(string[] args)
-        {
-            Write("Enter a number to calculate factorial : ");
-            int inputNumber = int.Parse(ReadLine());
-
-            WriteLine("{0} factorial is {1}", inputNumber, Factorial(inputNumber));
-            ReadLine();
-        }
-
-        public static double Factorial(int input)
-        {
-            double fact = 1;
-
-            while (input > 0)
-            {
-                fact = fact * input;
-                input--;
-            }
-
-            return fact;
-        }
+        fact = fact * input;
+        input--;
     }
+
+    return fact;
 }
