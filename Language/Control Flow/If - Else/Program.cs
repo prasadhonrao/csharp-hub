9df4e1a3348571aaf43587e5634a97b035aaf082
prasadhonrao <honrao.prasad@gmail.com>
2024-01-09 16:33:33 +0000

    Update if-else stmt app

--- Language/Control Flow/If - Else/Program.cs
+++ Language/Control Flow/If - Else/Program.cs
@@ -1,40 +1,19 @@
-﻿using System;
-using static System.Console;
+﻿using static System.Console;
 
-namespace Learn.CSharp.ControlFlow
+Console.WriteLine("If-Else Statement!");
+
+Write("Enter your name : ");
+string userName = ReadLine();
+
+if (DateTime.Now.Hour >= 8 && DateTime.Now.Hour < 12)
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Write("Enter your name : ");
-            string userName = ReadLine();
-
-            #region if - else
-
-            if (DateTime.Now.Hour >= 8 && DateTime.Now.Hour < 12)
-            {
-                WriteLine("Good morning {0}", userName);
-            }
-            else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour <= 17)
-            {
-                WriteLine("Good afternoon {0}", userName);
-            }
-            else
-            {
-                WriteLine("{0}, you're working odd hours", userName);
-            }
-            
-            #endregion
-
-            #region turnary operator ?
-
-            var is64BitOs = Environment.Is64BitOperatingSystem ? "Yes" : "No";
-            WriteLine("Is 64 bit operating system : {0}", is64BitOs); 
-            
-            #endregion
-
-            ReadLine();
-        }
-    }
+    WriteLine("Good morning {0}", userName);
+}
+else if (DateTime.Now.Hour >= 12 && DateTime.Now.Hour <= 17)
+{
+    WriteLine("Good afternoon {0}", userName);
+}
+else
+{
+    WriteLine("{0}, you're working odd hours", userName);
 }
