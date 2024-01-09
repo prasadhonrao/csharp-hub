d9b6f61d3ec883c6d5d9c06b5e730efd95f90294
prasadhonrao <honrao.prasad@gmail.com>
2024-01-09 16:40:41 +0000

    Update do...while loop solution

--- Language/Control Flow/Do - While/Program.cs
+++ Language/Control Flow/Do - While/Program.cs
@@ -1,29 +1,21 @@
 ﻿using static System.Console;
 
-namespace Learn.CSharp.ControlFlow
+Console.WriteLine("Do...While Loop!");
+
+Write("Enter a number to calculate factorial : ");
+int inputNumber = int.Parse(ReadLine());
+
+WriteLine("{0} factorial is {1}", inputNumber, Factorial(inputNumber));
+
+static double Factorial(int input)
 {
-    class Program
-    {   
-        static void Main(string[] args)
-        {
-            Write("Enter a number to calculate factorial : ");
-            int inputNumber = int.Parse(ReadLine());
-
-            WriteLine("{0} factorial is {1}", inputNumber, Factorial(inputNumber));
-            ReadLine();
-        }
-
-        public static double Factorial(int input)
-        {
-            double fact = 1;
-            do
-            {
-                fact = fact * input;
-                input--;
-            }
-            while (input > 0);
-
-            return fact;
-        }
+    double fact = 1;
+    do
+    {
+        fact = fact * input;
+        input--;
     }
+    while (input > 0);
+
+    return fact;
 }
